0cfb7b858d8586fd4ae03697d100e662eb98f5df
Yifan Wu <ywu@lendingclub.com>
2020-01-07 21:41:55 -0800

    Add Implementation for additive/multiplicative attention

--- I. Natural Language Processing/1. Effective Approaches to Attention-based Neural Machine Translation/mul-attn-tf2implementation.py
+++ I. Natural Language Processing/1. Effective Approaches to Attention-based Neural Machine Translation/mul-attn-tf2implementation.py
@@ -4,17 +4,23 @@ from tensorflow.keras.layers import *
 assert tf.__version__=="2.0.0", f"Expect TF-2.0.0 but get {tf.__version__}"
 
 class multiplicative_attention_model(tf.keras.Model):
-	"""A Tensorflow 2.0 implementation of encoder-decoder attention model as illustrated in 
-	Effective Approaches to Attention-based Neural Machine Translation.
+	"""
+	A Tensorflow 2.0 implementation of encoder-decoder attention model as illustrated in Effective Approaches to Attention-based Neural Machine Translation.
+	The following types of attentions are implemented:
+	• Global attention with dot alignment
+	• Local attention with monotonic alignment
 	
 	# Arguments:
 
+		attn_mode: `global` or `local`
 		input_shape: shape of input data, should be a two-dimensional tuple (batch, n_step)
 		input_vocab_size: size of input vocabulary size excluding <EOS>
 		output_vocab_size: size of output vocabulary size excluding <EOS>
 		input_embed_dim: word embedding dimension for input
-		hidden_state_dim: hidden state dimension for encoder and decoder GRU
-		alignment_dim: alignment score's internal dimension
+		hidden_state_dim: hidden state dimension for encoder and decoder LSTM
+		local_attn_window: the context vector will be extracted from window [current_position ± local_attn_window]
+		enc_lstm_dropout: fraction of the encoder lstm units to drop
+		dec_lstm_dropout: fraction of the decoder lstm units to drop
 		name: name of the model
 
 	# Paper:
@@ -25,12 +31,13 @@ class multiplicative_attention_model(tf.keras.Model):
 
 	```python
 		# Expected shape for input data and output data
+		attn_mode = "global"
 		input_shape = (None,40)
 		input_vocab_size = 200
 		output_vocab_size = 300
 
 		# Initialize Model
-		model = additive_attention_model(input_shape, input_vocab_size, output_vocab_size)
+		model = multiplicative_attention_model(attn_mode, input_shape, input_vocab_size, output_vocab_size)
 
 		# Compile Model
 		learning_rate = 1e-3
@@ -40,14 +47,18 @@ class multiplicative_attention_model(tf.keras.Model):
 					  )
 
 		# Call Model once to build the model
-		_ = model(tf.ones(shape=(1,20)))
+		_ = model(tf.ones(shape=(1,40)))
 
 		# Take a look at model
 		model.summary()
 	```
 	"""
-	def __init__(self, input_shape, input_vocab_size, output_vocab_size, name="Additive-Attention", input_embed_dim=200, hidden_state_dim=200, alignment_dim=40, **kwargs):
-		super(additive_attention_model, self).__init__(name=name, **kwargs)
+	def __init__(self, attn_mode, input_shape, input_vocab_size, output_vocab_size, name="Multiplicative-Attention", input_embed_dim=200, hidden_state_dim=200, local_attn_window=None, enc_lstm_dropout=0.2, dec_lstm_dropout=0.2, **kwargs):
+		super(multiplicative_attention_model, self).__init__(name=name, **kwargs)
+
+		# Expect attention model to be either global or local
+		assert attn_mode == "global" or attn_mode == "local", f"Expect 'global' or 'local' for attn_mode, get {attn_mode} instead"
+		self.attn_mode = attn_mode
 
 		# Expect input shape to be two-dimensional
 		assert isinstance(input_shape, tuple), "Expect tuple for input_shape, get {type(input_shape)} instead"
@@ -57,43 +68,68 @@ class multiplicative_attention_model(tf.keras.Model):
 		# Embedding layer for input
 		self._embedding_layer = Embedding(input_dim=input_vocab_size+1, output_dim=input_embed_dim, input_length=self._n_step)
 
-		# Bidirectional GRU encoder
-		self._bi_gru_encoder_layer = Bidirectional(GRU(units=hidden_state_dim, activation="tanh", recurrent_activation="sigmoid", use_bias=True, return_sequences=True, return_state=True), merge_mode="concat")
+		# LSTM encoder
+		self._lstm_encoder_layer_1 = LSTM(units=hidden_state_dim, activation="tanh", recurrent_activation="sigmoid", use_bias=True, return_sequences=True, return_state=True)
+		self._encoder_layer_1_dropout_layer = Dropout(enc_lstm_dropout)
+		self._lstm_encoder_layer_2 = LSTM(units=hidden_state_dim, activation="tanh", recurrent_activation="sigmoid", use_bias=True, return_sequences=True, return_state=True)
+		self._encoder_layer_2_dropout_layer = Dropout(enc_lstm_dropout)
 
-		# Weights and utility layers for calculating alignment score
-		self._W_a = self.add_weight(name="W_a", shape=(alignment_dim,hidden_state_dim), dtype=tf.float32)
-		self._U_a = self.add_weight(name="U_a", shape=(alignment_dim,hidden_state_dim*2), dtype=tf.float32)
-		self._V_a = self.add_weight(name="V_a", shape=(1,alignment_dim), dtype=tf.float32)
-		self._repeat_layer = RepeatVector(self._n_step)
-		self._additive_layer = Add()
+		# LSTM decoder
+		self._lstm_decoder_cell_1 = LSTMCell(units=hidden_state_dim, activation="tanh", recurrent_activation="sigmoid", use_bias=True)
+		self._decoder_cell_1_dropout_layer = Dropout(dec_lstm_dropout)
+		self._lstm_decoder_cell_2 = LSTMCell(units=hidden_state_dim, activation="tanh", recurrent_activation="sigmoid", use_bias=True)
+		self._decoder_dropout_layer = Dropout(dec_lstm_dropout)
+		self._decoder_state_array = tf.TensorArray(dtype=tf.float32, size=self._n_step, clear_after_read=True)
+
+		# Attention utility
+		if self.attn_mode == "global":
+			pass
+		if self.attn_mode == "local":
+			assert isinstance(local_attn_window, int), f"Expect integer for local_attn_window, get {type(local_attn_window)} instead"
+			assert local_attn_window > 0, "Expect positive value for local_attn_window"
+			self.D = local_attn_window
+
+		# Utility layer
 		self._concat_layer = Concatenate()
 
-		# GRU decoder
-		self._decoder_cell = GRUCell(units=hidden_state_dim, activation="tanh", recurrent_activation="sigmoid", use_bias=True)
-		self._decoder_state_array = tf.TensorArray(dtype=tf.float32,size=self._n_step)
+		# Output layer
 		self._ffnn_decoder_sequence_layer = TimeDistributed(Dense(output_vocab_size+1))
 	
-	@tf.function	
-	def call(self, inputs, training=True):
-
+	@tf.function
+	def call(self, inputs, training=False):
+		# Embedding
 		inputs_embed = self._embedding_layer(inputs)
-		encoder_sequence, forward_state, backward_state = self._bi_gru_encoder_layer(inputs_embed)
-		decoder_hidden_state = backward_state
-		encoder_sequence_transpose = tf.transpose(encoder_sequence, [1, 0, 2])
+
+		# Encoder
+		encoder_1_sequence, encoder_1_hidden_state, encoder_1_cell_state = self._lstm_encoder_layer_1(inputs=inputs_embed, training=training)
+		encoder_1_sequence = self._encoder_layer_1_dropout_layer(encoder_1_sequence, training=training)
+		encoder_2_sequence, encoder_2_hidden_state, encoder_2_cell_state = self._lstm_encoder_layer_2(inputs=encoder_1_sequence, initial_state=[encoder_1_hidden_state, encoder_1_cell_state], training=training)
+		encoder_2_sequence = self._encoder_layer_2_dropout_layer(encoder_2_sequence, training=training)
+		decoder_1_input = tf.transpose(encoder_2_sequence, [1, 0, 2])
+		decoder_1_states = [encoder_1_hidden_state, encoder_1_cell_state]
+		decoder_2_states = [encoder_2_hidden_state, encoder_2_cell_state]
+
+		# Decoder
 		for step in range(self._n_step):
-			W_a_dot_decoder_state_sequence = self._repeat_layer(tf.einsum("ij,kj->ik", decoder_hidden_state, self._W_a))
-			U_a_dot_encoder_state_sequence = tf.einsum("ijk,zk->ijz", encoder_sequence, self._U_a)
-			V_a_dot_tanh = tf.einsum("ijk,zk->ij", tf.nn.tanh(self._additive_layer([W_a_dot_decoder_state_sequence,U_a_dot_encoder_state_sequence])), self._V_a)
-			alignment_score = tf.nn.softmax(V_a_dot_tanh)
-			attention_vector = tf.einsum("ij,ijz->iz", alignment_score, encoder_sequence)
-			decoder_input = self._concat_layer([encoder_sequence_transpose[step], attention_vector])
-			decoder_hidden_state, _ = self._decoder_cell(inputs=decoder_input, states=[decoder_hidden_state])
-			self._decoder_state_array = self._decoder_state_array.write(step, decoder_hidden_state)
+			decoder_1_hidden_state, decoder_1_states = self._lstm_decoder_cell_1(inputs=decoder_1_input[step],states=decoder_1_states, training=training)
+			decoder_1_hidden_state = self._decoder_cell_1_dropout_layer(decoder_1_hidden_state, training=training)
+			decoder_2_hidden_state, decoder_2_states = self._lstm_decoder_cell_2(inputs=decoder_1_hidden_state, states=decoder_2_states, training=training)
+			if self.attn_mode == "global":
+				attention_score = tf.nn.softmax(tf.einsum("ijk,ik->ij", encoder_2_sequence, decoder_2_hidden_state))
+				context_vector = tf.einsum("ijk,ij->ik", encoder_2_sequence, attention_score)
+			if self.attn_mode == "local":
+				lb, ub = max(0, step-self.D), min(self._n_step-1,step+self.D)
+				encoder_2_sequence_part = encoder_2_sequence[:,lb:ub+1,:]
+				attention_score = tf.nn.softmax(tf.einsum("ijk,ik->ij", encoder_2_sequence_part, decoder_2_hidden_state))
+				context_vector = tf.einsum("ijk,ij->ik", encoder_2_sequence_part, attention_score)
+			output_vector = self._concat_layer([decoder_2_hidden_state, context_vector])
+			self._decoder_state_array = self._decoder_state_array.write(step,output_vector)
 		decoder_sequence = tf.transpose(self._decoder_state_array.stack(), [1, 0, 2])
+		decoder_sequence = self._decoder_dropout_layer(decoder_sequence, training=training)
+
+		# Softmax outputs
 		outputs = tf.nn.softmax(self._ffnn_decoder_sequence_layer(decoder_sequence))
 		return outputs
-
-
 
 
 
--- I. Natural Language Processing/2. Neural Machine Translation by Jointly Learning to Align and Translate/add-attn-tf2implementation.py
+++ I. Natural Language Processing/2. Neural Machine Translation by Jointly Learning to Align and Translate/add-attn-tf2implementation.py
@@ -15,6 +15,8 @@ class additive_attention_model(tf.keras.Model):
 		input_embed_dim: word embedding dimension for input
 		hidden_state_dim: hidden state dimension for encoder and decoder GRU
 		alignment_dim: alignment score's internal dimension
+		enc_gru_dropout: fraction of the encoder gru units to drop
+		dec_gru_dropout: fraction of the decoder gru units to drop
 		name: name of the model
 
 	# Paper:
@@ -40,13 +42,13 @@ class additive_attention_model(tf.keras.Model):
 					  )
 
 		# Call Model once to build the model
-		_ = model(tf.ones(shape=(1,20)))
+		_ = model(tf.ones(shape=(1,40)))
 
 		# Take a look at model
 		model.summary()
 	```
 	"""
-	def __init__(self, input_shape, input_vocab_size, output_vocab_size, name="Additive-Attention", input_embed_dim=200, hidden_state_dim=200, alignment_dim=40, **kwargs):
+	def __init__(self, input_shape, input_vocab_size, output_vocab_size, name="Additive-Attention", input_embed_dim=200, hidden_state_dim=200, alignment_dim=40, enc_gru_dropout=0.2, dec_gru_dropout=0.2, **kwargs):
 		super(additive_attention_model, self).__init__(name=name, **kwargs)
 
 		# Expect input shape to be two-dimensional
@@ -59,6 +61,7 @@ class additive_attention_model(tf.keras.Model):
 
 		# Bidirectional GRU encoder
 		self._bi_gru_encoder_layer = Bidirectional(GRU(units=hidden_state_dim, activation="tanh", recurrent_activation="sigmoid", use_bias=True, return_sequences=True, return_state=True), merge_mode="concat")
+		self._encoder_dropout_layer = Dropout(enc_gru_dropout)
 
 		# Weights and utility layers for calculating alignment score
 		self._W_a = self.add_weight(name="W_a", shape=(alignment_dim,hidden_state_dim), dtype=tf.float32)
@@ -70,16 +73,22 @@ class additive_attention_model(tf.keras.Model):
 
 		# GRU decoder
 		self._decoder_cell = GRUCell(units=hidden_state_dim, activation="tanh", recurrent_activation="sigmoid", use_bias=True)
-		self._decoder_state_array = tf.TensorArray(dtype=tf.float32,size=self._n_step)
+		self._decoder_dropout_layer = Dropout(dec_gru_dropout)
+		self._decoder_state_array = tf.TensorArray(dtype=tf.float32, size=self._n_step, clear_after_read=True)
 		self._ffnn_decoder_sequence_layer = TimeDistributed(Dense(output_vocab_size+1))
 	
 	@tf.function	
-	def call(self, inputs, training=True):
-
+	def call(self, inputs, training=False):
+		# Embedding
 		inputs_embed = self._embedding_layer(inputs)
-		encoder_sequence, forward_state, backward_state = self._bi_gru_encoder_layer(inputs_embed)
+
+		# Encoder
+		encoder_sequence, forward_state, backward_state = self._bi_gru_encoder_layer(inputs_embed, training=training)
+		encoder_sequence = self._encoder_dropout_layer(encoder_sequence, training=training)
 		decoder_hidden_state = backward_state
 		encoder_sequence_transpose = tf.transpose(encoder_sequence, [1, 0, 2])
+
+		# Decoder
 		for step in range(self._n_step):
 			W_a_dot_decoder_state_sequence = self._repeat_layer(tf.einsum("ij,kj->ik", decoder_hidden_state, self._W_a))
 			U_a_dot_encoder_state_sequence = tf.einsum("ijk,zk->ijz", encoder_sequence, self._U_a)
@@ -87,15 +96,11 @@ class additive_attention_model(tf.keras.Model):
 			alignment_score = tf.nn.softmax(V_a_dot_tanh)
 			attention_vector = tf.einsum("ij,ijz->iz", alignment_score, encoder_sequence)
 			decoder_input = self._concat_layer([encoder_sequence_transpose[step], attention_vector])
-			decoder_hidden_state, _ = self._decoder_cell(inputs=decoder_input, states=[decoder_hidden_state])
+			decoder_hidden_state, _ = self._decoder_cell(inputs=decoder_input, states=[decoder_hidden_state], training=training)
 			self._decoder_state_array = self._decoder_state_array.write(step, decoder_hidden_state)
 		decoder_sequence = tf.transpose(self._decoder_state_array.stack(), [1, 0, 2])
+		decoder_sequence = self._decoder_dropout_layer(decoder_sequence, training=training)
+
+		# Softmax outputs
 		outputs = tf.nn.softmax(self._ffnn_decoder_sequence_layer(decoder_sequence))
 		return outputs
-
-
-
-
-
-
-
